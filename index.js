479b8f999dcbe13aae9eee1bae124e91769cec84
Jeff Jiang <jeffjiang8@gmail.com>
2020-05-20 18:21:12 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Given a string S, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
 
 var removeVowels = function(S) {
-    
+    return S.replace(/[aeiou-]/g,'')
 };
